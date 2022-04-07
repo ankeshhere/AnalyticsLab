309f40f340e6150f6a8f071fda486672838383fa
Minal <minalprakash1997@gmail.com>
2022-04-07 21:50:07 +0530

    added header and card files
    added header and card files

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from '../src/components/Header';
+import Card from '../src/components/Card';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <div>
+      <Card />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import classes from './Header.module.css';
+
+const Header = () => {
+return (
+    <header className={classes.header}>
+        <h3>Box App</h3>
+        {/* <nav></nav> */}
+    </header>
+);
+
+};
+export default Header;
